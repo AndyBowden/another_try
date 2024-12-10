2f5564e79b646b505bcdacd4b26c39f5f0cc9d38
AndyBowden <59970701+AndyBowden@users.noreply.github.com>
2024-12-10 11:17:53 +0000

    Update ecoflow.py

--- custom_components/powerocean/ecoflow.py
+++ custom_components/powerocean/ecoflow.py
@@ -495,7 +495,9 @@ class Ecoflow:
         
     def _get_serial_numbers(self, response):
       
-        p = response["data"]["parallelx"]
+        # p = response["data"]["parallel"]
+        p = response["data"]["nonsense"]
+        _LOGGER.debug(f"reponse__{p}")
         _LOGGER.debug(f"parallel_present__{len(p)}")
 
         if len(p) == 0 :
